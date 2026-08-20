896501128fec2687b2c4a2e2d33a598413a7b5a7
Akshita gupta <130370469+Akshitagupta299@users.noreply.github.com>
2026-08-20 11:10:21 +0530

    Update database.py

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -3,30 +3,13 @@ from app.config.settings import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-
-database_url = settings.DATABASE_URL
-
-if database_url.startswith("postgres://"):
-    database_url = database_url.replace(
-        "postgres://",
-        "postgresql+psycopg2://",
-        1
-    )
-elif database_url.startswith("postgresql://"):
-    database_url = database_url.replace(
-        "postgresql://",
-        "postgresql+psycopg2://",
-        1
-    )
-
-engine = create_engine(database_url)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(
     bind=engine,
     autoflush=False,
     autocommit=False
 )
-
 Base = declarative_base()
 
 
